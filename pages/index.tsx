95e10d9e79b7bb9b76373f69346ae6def661887b
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-11-24 17:41:51 +0100

    [ADD]All

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,51 +4,20 @@ import { useRef, useState } from "react"
 import { useLocomotiveScroll } from 'react-locomotive-scroll'
 import Tools from "../components/tools/Tools"
 import Cases from "../components/cases/Cases"
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 const Home = () => {
     const { scroll } = useLocomotiveScroll()
-    const containerRef = useRef(null)
 
 
     return (
-        <LocomotiveScrollProvider
-            options={
-                {
-                    smooth: true,
-                    // ... all available Locomotive Scroll instance options 
-                }
-            }
-            watch={
-                [
-                    //..all the dependencies you want to watch to update the scroll.
-                    //  Basicaly, you would want to watch page/location changes
-                    //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-                ]
-            }
-            containerRef={containerRef}>
-            <main style={{ position: "fixed" }} ref={containerRef} data-scroll-container>
-                <div data-scroll-section>
-                    <Nav />
-
-                </div>
-                <div data-scroll-section>
-                    <Header />
-
-                </div>
-                <div data-scroll-section>
-                    <Tools />
-
-                </div>
-                <div data-scroll-section>
-                    <Cases />
-
-                </div>
+        <div data-scroll-section>
+            <Nav />
+            <Header />
+            <Tools />
+            <Cases />
+        </div>
 
 
-
-            </main>
-        </LocomotiveScrollProvider >
 
     )
 }
